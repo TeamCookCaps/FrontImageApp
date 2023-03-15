c61b81d820b5d7d763df15073c48ab3df72a7341
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2023-03-15 20:37:09 +0900

    [#1] router setting

---
+++ src/pages/LikePhoto.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function LikePhoto() {
+  return <div>LikePhoto Page</div>;
+}
---
+++ src/pages/RecommandPhoto.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function RecommandPhoto() {
+  return <div>RecommandPhoto Page</div>;
+}
