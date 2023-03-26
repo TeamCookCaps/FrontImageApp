964751c05c899710abd7938f849b73c0547955f6
zjaym1703 <jym971024@gmail.com>
2023-03-26 23:09:39 +0900

    [#8] 이미지 업로드 시 blob 파일 unwrapping

--- src/api/uploader.js
+++ src/api/uploader.js
@@ -5,9 +5,7 @@ export async function uploadImage(uid, files) {
     const formData = new FormData();
     formData.append('uid', uid);
     for (let i = 0; i < files.length; i++) {
-      const fileJson = JSON.stringify(files[i]);
-      const blob = new Blob([fileJson], { type: 'application/json' });
-      formData.append('files', blob);
+      formData.append('file_list', files[i]);
     }
 
     // console에서 데이터 확인용
@@ -17,7 +15,7 @@ export async function uploadImage(uid, files) {
 
     // 서버에 데이터 전송
     const response = await axios.post(
-      'http://localhost:3000/api/upload',
+      'http://localhost:8080/api/image_upload',
       formData
     );
     console.log(response.data);
