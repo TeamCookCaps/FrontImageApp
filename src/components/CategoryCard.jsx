a39fa4e68de9db3cd624ace20ca0600f58ec0741
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2023-03-30 21:35:09 +0900

    [#27] 좋아요 아이콘 추가

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,26 +1,40 @@
 import React, { useState } from 'react';
 import { FaEllipsisV } from 'react-icons/fa';
+import { HiHeart } from 'react-icons/hi';
 
-export default function CategoryCard({ photo: { id, image_url } }) {
+export default function CategoryCard({
+  photo: { id, image_url },
+  numOfPhotos,
+  isFavorite,
+}) {
   const [hovered, setHovered] = useState(false);
   return (
-    <li className="flex flex-col pb-10">
-      <img
-        className={`w-48 h-48 object-cover rounded-xl shadow-md transition duration-300 ${
-          hovered ? 'scale-95' : 'scale-100'
-        } hover:cursor-pointer hover:ease-out`}
-        src={image_url} // 임시
-        alt={id} // 임시
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
-      />
-      <div className="w-48 flex items-center justify-between pt-3 pl-1">
-        <p className="font-medium text-xl text-gray-900">{id}</p>
-        <FaEllipsisV className="text-lg" />
+    <li className="flex flex-col pb-10 shrink-0">
+      <div className="relative w-48 h-48">
+        <img
+          className={`w-48 h-48 object-cover rounded-xl shadow-md transition duration-300 ${
+            hovered ? 'scale-95' : 'scale-100'
+          } hover:cursor-pointer hover:ease-out`}
+          src={image_url} // 임시
+          alt={id} // 임시
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+        />
+        {isFavorite && (
+          <HiHeart className="absolute top-3 right-4 text-4xl text-red-400 hover:cursor-pointer" /> // 임시
+        )}
       </div>
-      {/* <span className="pl-1 text-lg text-gray-400 font-normal">
-        {numOfPictures}
-      </span> */}
+      {!isFavorite && (
+        <>
+          <div className="w-48 flex items-center justify-between pt-3 pl-1">
+            <p className="font-medium text-xl text-gray-900">{id}</p>
+            <FaEllipsisV className="text-lg" />
+          </div>
+          <span className="pl-1 text-lg text-gray-400 font-normal">
+            {numOfPhotos}
+          </span>
+        </>
+      )}
     </li>
   );
 }
