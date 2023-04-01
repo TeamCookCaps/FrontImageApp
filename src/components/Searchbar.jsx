069a11d847e8a1c6b6f5d9e1be1dcb8698e7c56e
zjaym1703 <jym971024@gmail.com>
2023-04-02 00:59:41 +0900

    [#20] color picker 에서 클릭 시 검색 창 옆에 선택 색상 보여주도록 구현

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,37 +1,58 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BiSearch, BiPalette } from 'react-icons/bi';
 import { useNavigate } from 'react-router-dom';
+import Compact from '@uiw/react-color-compact';
 
 export default function Searchbar(){
     const [text, setText] = useState('');
+    const [display, setDisplay] = useState(false);
+    const [hex, setHex] = useState('#fffff');
+
     const navigate = useNavigate();
 
     const handleChange = (e) => setText(e.target.value);
     const handleOnKeyDown = (e) => {
         if(e.code === 'Enter' && e.nativeEvent.isComposing === false){
-          navigate('/search',{state:{searchWord : `${text}`}});
-          console.log('dsf')
+          navigate('/search',{state:{ searchWord : `${text}`}});
           setText('')
         }
     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const displayClick = () => {
+        setDisplay(!display)
     };
-
+    
+    const displayClose = () => {
+        setDisplay(false)
+    }
     return(
-        <form
-        className="w-4/5 flex items-center gap-5 mr-10"
-        onSubmit={handleSubmit}>
-            <BiSearch className="text-3xl" />
-            <input
-                className="w-full outline-none text-xl"
-                type="text"
-                value={text}
-                placeholder="Search photo"
-                onChange={handleChange}
-                onKeyDown={handleOnKeyDown}/>
-            <BiPalette className="text-3xl"/>
-        </form>
+        <ul className='w-4/5'>
+            <li>
+            <form
+            className="flex items-center gap-3 mr-10"
+            onSubmit={(e)=> { e.preventDefault(); }}>
+                <BiSearch className="text-3xl" />
+                <input
+                    className="w-full outline-none text-xl"
+                    type="text"
+                    value={text}
+                    placeholder="검색하려는 카테고리와 위치를 입력하세요. 카테고리는 영어로만 검색 가능합니다"
+                    onChange={handleChange}
+                    onKeyDown={handleOnKeyDown}/>
+                <div className="w-6 h-5 rounded-full" 
+                    style={{backgroundColor : hex}}/>
+                <BiPalette className="text-3xl" onClick={displayClick}/>
+                </form>
+            </li>
+            <li>
+            {display ? <div className='absolute z-2'>
+                <Compact
+                    className='w-150 h-100'
+                    color={hex}
+                    onChange={(color)=> { setHex(color.hex); }}/>
+            </div> : null}
+            </li>
+        </ul>  
+       
     )
 }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { search } from '../api/search';
 
 const searchList = [
   {
@@ -31,6 +33,20 @@ export default function Search() {
   const location = useLocation();
   const searchWord = location.state.searchWord;
 
+  const { isLoading,isFetching, error, data : result } = useQuery({
+    queryKey : ['data'],
+    queryFn : () => search(searchWord)
+  }); 
+  if(isLoading || isFetching){
+    return (<div>로딩 중</div>)
+  }
+  if(error) {
+    return (<div>에러가 발생했습니다 : {error}</div>)
+  }
+
+  if(result){
+    console.log(result);
+  }
   return (
     <section className="flex flex-col gap-5 py-5 px-5">
       <section className="mb-4">
@@ -39,13 +55,14 @@ export default function Search() {
         </header>
       </section>
       <section>
+      {result.length === 0 && <div> 검색 결과가 없습니다! </div>}
         <div className="grid grid-cols-1 gap-y-5 gap-x-5 sm:grid-cols-4 lg:grid-cols-6 xl:gap-cols-8">
-          {searchList.map((search) => (
-            <div key={search.id} className="group relative">
+          {result && result?.map((search) => (
+            <div key={search?.id} className="group relative">
               <div className="min-h-200 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-40">
-                <a href={search.href}>
+                <a href={search?.image_url}>
                 <img
-                  src={search.imageSrc}
+                  src={search?.image_url}
                   className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                 />
                 </a>
