c25b4b3897fda6577d050235d8028be0276bb454
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2023-04-07 18:23:28 +0900

    [#37] Uploading Modal 추가

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { uploadImage } from '../api/database';
+import UploadingModal from './UploadingModal';
 
 export default function Upload({ setShowModal, user: { uid } }) {
   const [files, setFiles] = useState([]);
+  const [isUploading, setIsUploading] = useState(false);
 
   const handleChange = (e) => {
     const selectedFiles = Array.from(e.target.files);
@@ -10,14 +12,20 @@ export default function Upload({ setShowModal, user: { uid } }) {
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsUploading(true);
     uploadImage(uid, files) // 이미지 업로드 함수 호출
       .then((res) => {
         console.log(res);
+      })
+      .finally(() => {
+        setIsUploading(false);
+        setShowModal(false);
       });
   };
 
   return (
     <section className="fixed top-0 left-0 w-full h-full bg-gray-500 bg-opacity-50 flex justify-center items-center">
+      {isUploading && <UploadingModal />}
       <div className="w-4/6 h-5/6 overflow-y-auto bg-white rounded-lg p-8 flex flex-col">
         <form onSubmit={handleSubmit} className="flex-grow">
           <h2 className="text-2xl font-bold mb-4">Upload Image</h2>
@@ -43,11 +51,15 @@ export default function Upload({ setShowModal, user: { uid } }) {
           <div className="flex justify-end mt-4">
             <button
               className="bg-red-400 text-white px-4 py-2 rounded-lg mr-4"
+              disabled={isUploading}
               onClick={() => setShowModal(false)}
             >
               Cancel
             </button>
-            <button className="bg-yellow-400 text-white px-4 py-2 rounded-lg">
+            <button
+              className="bg-yellow-400 text-white px-4 py-2 rounded-lg"
+              disabled={isUploading}
+            >
               Upload
             </button>
           </div>
