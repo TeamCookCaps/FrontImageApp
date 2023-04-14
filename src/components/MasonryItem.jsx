01a640309d3cc4eb78578940260f4f5bbb34aca0
zjaym1703 <jym971024@gmail.com>
2023-04-15 02:03:13 +0900

    [#43] 이미지 로딩 처리

--- src/components/MasonryItem.jsx
+++ src/components/MasonryItem.jsx
@@ -10,7 +10,9 @@ export default function MasonryItem({ search, isShow ,info }) {
 
   const [like, setLike] = useState(favorite_yn);
   const [hovered, setHovered] = useState(false);
-  const likeMutate = useLike();
+  const [load,setLoad] = useState(false);
+
+  const likeMutate = useLike('data'); // data가 업데이트 키
 
   // const queryClient = useQueryClient();
   // const mutation = useMutation(transFavorite, { 
@@ -36,6 +38,10 @@ export default function MasonryItem({ search, isShow ,info }) {
     likeMutate.mutate({ uid : user?.uid ,id : id});
   }
 
+  const handleImageLoad = () => {
+    setLoad(true);
+  };
+
   return (
     <div className='relative w-full p-3'>
       <div className='w-full overflow-hidden rounded-md'
@@ -55,11 +61,17 @@ export default function MasonryItem({ search, isShow ,info }) {
           </ul>
           
           <img 
-              className={`h-full w-full object-cover object-center lg:h-full lg:w-full ${hovered ? 'scale-95' : 'scale-100' }hover:cursor-pointer hover:ease-out`}
+              className={`${load ? '' : 'hidden'} h-full w-full object-cover object-center lg:h-full lg:w-full ${hovered ? 'scale-95' : 'scale-100' }hover:cursor-pointer hover:ease-out`}
               src={search?.image_url}
               alt={search?.id}
+              onLoad={handleImageLoad}
               />
-          
+          {!load && (
+            <div className='animate-pulse flex flex-col'>
+              <div className="rounded w-full h-52 bg-gray-200"></div>
+            </div>
+            
+          )}
       </div>
     </div>
   );
--- src/hook/useLike.js
+++ src/hook/useLike.js
@@ -2,13 +2,13 @@ import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { transFavorite } from '../api/favorite';
 
 
-export const useLike = () => {   
+export const useLike = (updateKey) => {   
     const queryClient = useQueryClient();
     return useMutation(transFavorite, { 
         onSettled : (data) => {
             // if(data.data === "like") setLike((data) => data = 'y');
             // else setLike((data) => data = 'n');
-            queryClient.invalidateQueries('data');
+            queryClient.invalidateQueries(updateKey); //search 페이지에서 data 로 불러옴 
             
         },
         onError : (e)=>{
