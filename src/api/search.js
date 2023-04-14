893c3375c6fd776d7c8d327dbdc3b6b959bf139b
zjaym1703 <jym971024@gmail.com>
2023-04-14 23:04:02 +0900

    [#43] search api get 메소드로 변경

--- src/api/search.js
+++ src/api/search.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-export async function search(word,color) {
-    console.log('호출 확인')
-    return axios.post('http://localhost:4000/api/search',{ word : word, color:color })
+export async function search(uid, word,color) {
+    return axios.get(`http://localhost:4000/api/search`,{ params : { uid : uid, word: word, color:color} })
                 .then((res) => res.data.data)
                 .catch((error) => error)
 }
