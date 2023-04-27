63abbea2eacc66528e17cb49b1179ab734a34b8c
skc4234 <skc4234@naver.com>
2023-04-27 20:16:09 +0900

    [#52] /recommand 입력 시 추천 페이지 이동 및 조회

--- src/pages/RecommandPhoto.jsx
+++ src/pages/RecommandPhoto.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { getRecommandImage } from '../api/recommand';
+import { useAuthContext } from '../context/AuthContext';
 
 // 임시 데이터
 const recommands = [
@@ -42,6 +45,11 @@ const recommands = [
 ]
 
 export default function RecommandPhoto() {
+
+  const { user } = useAuthContext();
+  
+  const { isLoading, isFetching, error, data : result } = useQuery(['recommandImage'], () => getRecommandImage(user.uid));
+
   return (
     <div className="bg-white">
     
@@ -50,22 +58,22 @@ export default function RecommandPhoto() {
         <div className="space-x-2 flex justify-end">
         </div>
         <div className="mt-6 grid grid-rows-1 gap-y-10 gap-x-6 sm:grid-rows-2 lg:grid-rows-4 xl:gap-x-8">
-          {recommands.map((recommand) => (
-            <div key={recommand.photoId} className="group relative">
+          {result && result?.map((recommand) => (
+            <div key={recommand.id} className="group relative">
               <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
                 <img
-                  src={recommand.imageSrc}
-                  alt={recommand.imageAlt}
+                  src={recommand.image_url}
+                  alt="recommandImage"
                   className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                 />
               </div>
               <div className="mt-0 flex justify-between">
                 <div>
                   <h3 className="text-sm text-gray-700">
-                    <a href={recommand.href}>
+                    {/* <a href={recommand.href}>
                       <span aria-hidden="true" className="absolute inset-0" />
                       <p className="mt-1 text-sm text-gray-500"># {recommand.category}</p>
-                    </a>
+                    </a> */}
                   </h3>
                 </div>
               </div>
