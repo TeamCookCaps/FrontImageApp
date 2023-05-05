fd69b8858adf26b9970280ea6723320a5979ffd9
zjaym1703 <jym971024@gmail.com>
2023-05-06 01:45:53 +0900

    [#55] user profile 추가

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -8,7 +8,16 @@ export default function UserPage() {
 
     return(
         <>
-            
+            <section className='px-4'>
+                <div className="flex flex-col space-y-4 md:space-y-0 md:space-x-6 md:flex-row">
+                    <img src={user.profile_img}
+                        className="self-center flex-shrink-0 w-24 h-24 border rounded-full md:justify-self-start dark:bg-gray-500 dark:border-gray-700" />
+                    <div className="flex flex-col">
+                        <h1 className="text-xl font-semibold md:text-left">{user.nick_name}</h1>
+                        {/* <button className='flex'>커미션</button> */}
+                    </div>                        
+                </div>
+            </section>
             <Main user={user}/>
         </>
     )
