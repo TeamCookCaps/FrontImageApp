72f7161874170275c5930c316f3337ab7a621e4f
chaejin <paster12@naver.com>
2023-05-06 18:30:44 +0900

    [#36] 스토리탭 화면

--- src/pages/Story.jsx
+++ src/pages/Story.jsx
@@ -1,5 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useAuthContext } from '../context/AuthContext';
+import { BsPlusCircleFill } from 'react-icons/bs';
+import Upload from '../components/Upload';
 
 export default function Story() {
-  return <div>Story Page</div>;
+  const { user } = useAuthContext();
+  const [showModal, setShowModal] = useState(false);
+  //const story_yn = 'Y';
+  
+  return (
+	<>
+	<div class="container">
+
+		<div class="gallery">
+
+			<div class="gallery-item" tabindex="0">
+
+				<img src="https://images.unsplash.com/photo-1511765224389-37f0e77cf0eb?w=500&h=500&fit=crop" class="gallery-image" alt=""/>
+
+			</div>
+
+			<div class="gallery-item" tabindex="0">
+
+				<img src="https://images.unsplash.com/photo-1497445462247-4330a224fdb1?w=500&h=500&fit=crop" class="gallery-image" alt=""/>
+
+			</div>
+
+			<div class="gallery-item" tabindex="0">
+
+				<img src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?w=500&h=500&fit=crop" class="gallery-image" alt=""/>
+
+			</div>
+
+			<div class="gallery-item" tabindex="0">
+
+				<img src="https://images.unsplash.com/photo-1502630859934-b3b41d18206c?w=500&h=500&fit=crop" class="gallery-image" alt=""/>
+
+			</div>
+
+			<div class="gallery-item" tabindex="0">
+
+				<img src="https://images.unsplash.com/photo-1498471731312-b6d2b8280c61?w=500&h=500&fit=crop" class="gallery-image" alt=""/>
+
+			</div>
+
+			<div class="gallery-item" tabindex="0">
+
+				<img src="https://images.unsplash.com/photo-1515023115689-589c33041d3c?w=500&h=500&fit=crop" class="gallery-image" alt=""/>
+
+			</div>
+
+			<div class="gallery-item" tabindex="0">
+
+				<img src="https://images.unsplash.com/photo-1504214208698-ea1916a2195a?w=500&h=500&fit=crop" class="gallery-image" alt=""/>
+
+			</div>
+
+			<div class="gallery-item" tabindex="0">
+
+				<img src="https://images.unsplash.com/photo-1515814472071-4d632dbc5d4a?w=500&h=500&fit=crop" class="gallery-image" alt=""/>
+
+			</div>
+
+			<div class="gallery-item" tabindex="0">
+
+				<img src="https://images.unsplash.com/photo-1511407397940-d57f68e81203?w=500&h=500&fit=crop" class="gallery-image" alt=""/>
+
+			</div>
+
+		</div>
+		
+
+		<div class="loader"></div>
+
+	</div>
+	<BsPlusCircleFill
+            className="text-7xl fixed bottom-10 right-10 hover:cursor-pointer"
+            onClick={() => setShowModal(true)}
+          />
+	{showModal && <Upload setShowModal={setShowModal} user={user} /*story_yn={story_yn}*//>}
+	</>
+  )
 }
