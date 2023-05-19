defdd1ef6853433668b19258a10e32667e37cdeb
kmchaejin <cowls060@hansung.ac.kr>
2023-05-19 20:24:08 +0900

    [#36] 갤러리탭 description 출력

--- src/api/gallery.js
+++ src/api/gallery.js
@@ -1,8 +1,19 @@
 import axios from 'axios';
 
 export async function getGalleryImage(uid) {
-    console.log('gallery 호출');
     return axios.post('http://localhost:4000/api/gallery',{ uid })
+                .then((res) => res.data.data)
+                .catch((error) => error)
+}
+
+export async function saveImageDescription(id, description) {
+    return axios.post('http://localhost:4000/api/saveDescription',{ id, description })
+                .then((res) => res.data.data)
+                .catch((error) => error)
+}
+
+export async function getImageDescription(id) {
+    return axios.post('http://localhost:4000/api/getDescription',{ id })
                 .then((res) => res.data.data)
                 .catch((error) => error)
 }
