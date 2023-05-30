62851b8f452a6b2f8f0bb26c77c6c3292cb962a4
kmchaejin <cowls060@hansung.ac.kr>
2023-05-30 13:32:58 +0900

    [#81] 유저페이지 접근 오류 해결

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -5,16 +5,18 @@ import ChatModal from '../components/ChatModal';
 import { useQuery } from '@tanstack/react-query';
 import { getGalleryImage } from '../api/gallery';
 import GalleryCard from '../components/GalleryCard';
+import Masonry from 'react-masonry-css';
+
 
 export default function UserPage() {
   const {
     state: { user },
-  } = useLocation();  
+  } = useLocation();
   console.log(user);
-  
+
   const [showModal, setShowModal] = useState(false);
 
-  const { isLoading, isFetching, error, data : gallery_result } = useQuery(
+  const { isLoading, isFetching, error, data: galleryImages } = useQuery(
     ['galleryImage'], () => getGalleryImage(user.uid),
   );
 
@@ -27,25 +29,36 @@ export default function UserPage() {
             alt={user.nick_name}
             className="self-center flex-shrink-0 w-24 h-24 border rounded-full md:justify-self-start dark:bg-gray-500 dark:border-gray-700"
           />
-            <h1 className="text-xl font-semibold md:text-left">
-              {user.nick_name}
-            </h1>
-            <button onClick={() => setShowModal(true)} className='bg-yellow-300 hover:bg-yellow-500 text-black font-bold py-2 px-4 rounded'>커미션</button>
+          <h1 className="text-xl font-semibold md:text-left">
+            {user.nick_name}
+          </h1>
+          <button onClick={() => setShowModal(true)} className='bg-yellow-300 hover:bg-yellow-500 text-black font-bold py-2 px-4 rounded'>커미션</button>
         </div>
       </section>
       <Main user={user} />
       <section>
-      <h2 className="pb-7 text-3xl font-bold">Gallery</h2>
-        <div class=/*"flex space-x-4 overflow-x-auto p-4"*/"flex flex-wrap p-4 -mx-4"> 
-            {gallery_result && gallery_result?.map((gallerycard) => (
-            <div class="w-full sm:w-1/3 md:w-1/3 lg:w-1/3 xl:w-1/3 px-4 mb-6">
-            <GalleryCard key={gallerycard.image_id} gallerycard={gallerycard}/>
-            </div>
+        <h2 className="pb-7 text-3xl font-bold">Gallery</h2>
+        <Masonry
+          className="flex animate-slide-fwd"
+          breakpointCols={{
+            default: 3,
+            1300: 2,
+            900: 1,
+          }}
+        >
+          {galleryImages &&
+            galleryImages.map((galleryImage) => (
+              <div class="w-full sm:w-1/3 md:w-1/3 lg:w-1/3 xl:w-1/3 px-4 mb-6">
+                <GalleryCard
+                  key={galleryImage.image_id}
+                  galleryImage={galleryImage}
+                />
+              </div>
             ))}
-        </div>
+        </Masonry>
       </section>
-      
-      {showModal && <ChatModal setShowModal={setShowModal} receiver={user}/>}
+
+      {showModal && <ChatModal setShowModal={setShowModal} receiver={user} />}
     </>
   );
 }
